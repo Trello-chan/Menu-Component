6e282e930edde2ebb0817a880f977c998a749a1f
Kin Chan <xkinchanx@gmail.com>
2019-02-26 11:26:43 -0800

    [feat] front end skeleton

---
+++ client/src/components/MenuService.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class MenuService extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        hello
+      </div>
+    )
+  }
+}
+
+export default MenuService;
---
+++ client/src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render } from 'react-dom';
+import MenuService from './components/MenuService';
+
+render(<MenuService />, document.getElementById('menuService'));
