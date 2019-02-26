43f54ca05b5ec3b41aeab55d2191ef1e6e54d740
Kin Chan <xkinchanx@gmail.com>
2019-02-26 11:35:20 -0800

    [style] global style to ensure correct styling with main service styles

--- client/src/components/MenuService.jsx
+++ client/src/components/MenuService.jsx
@@ -1,20 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
 
-class MenuService extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+const MenuService = () =>
+  <StyledMenuService>
+    <GlobalStyle />
+    hello
+  </StyledMenuService>
 
-    }
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: blue;
+    height: 100vh;
+    margin: 0px;
   }
+`;
 
-  render() {
-    return (
-      <div>
-        hello
-      </div>
-    )
-  }
-}
+const StyledMenuService = styled.div`
+  background: rgba(0,0,0,.24);
+  display: flex;
+  flex-direction: row;
+  height: 40px;
+  margin-bottom: 8px;
+  width: 100%;
+`;
+
+
 
 export default MenuService;
