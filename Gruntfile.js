9eaf9e00da7ed751eb30867cef4a62a9c22cb405
Kin Chan <xkinchanx@gmail.com>
2019-02-28 15:59:51 -0800

    [ops] trying to set up grunt to automate bundle to s3, need to fix s3 permissions

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+  // Load S3 plugin
+  grunt.loadNpmTasks('grunt-aws');
+  // Static Webserver
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    aws: grunt.file.readJSON('.aws.json'),
+    s3: {
+      options: {
+        accessKeyId: "<%= aws.accessKeyId %>",
+        secretAccessKey: "<%= aws.secretAccessKey %>",
+        bucket: "<%= aws.bucket %>"
+      },
+      build: {
+        cwd: "public",
+        src: "**"
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          base: "client/dist/",
+          keepalive: true
+        }
+      }
+    }
+  });
+  // Default task(s).
+  grunt.registerTask("default", ["connect"]);
+
+};
